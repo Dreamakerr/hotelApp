4478578b62ccc8f0c1e9842506501a9f077822ea
SoRrY994 <2265904238@qq.com>
2024-05-10 17:07:20 +0800

    完成会员本月消费统计

---
+++ hotelManagementApp.DAL/viewExpendRecord.cs
@@ -0,0 +1,26 @@
+﻿using hotelManagementApp.DAL.Base;
+using hotelManagementApp.Models.VModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotelManagementApp.DAL
+{
+    public class viewExpendRecord:BQuery<vExpendRecord>
+    {
+        /// <summary>
+        /// 查询当月的所有消费记录
+        /// </summary>
+        /// <returns></returns>
+        public List<vExpendRecord> getThisMonthExpendList()
+        {
+            string stTime = DateTime.Now.Year + "-" + DateTime.Now.Month.ToString("00") + "-01 00:00:00";
+            DateTime et = DateTime.Now.AddMonths(1);
+            string etTime = et.Year + "-" + et.Month.ToString("00") + "-01 00:00:00";
+            string strWhere = $"isDeleted=0 and expendTime>='{stTime}' and expendTime<'{etTime}'";
+            return GetModelList(strWhere, "", "");
+        }
+    }
+}
---
+++ hotelManagementApp.Models/VModels/vExpendRecord.cs
@@ -0,0 +1,25 @@
+﻿using common.CustAttributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotelManagementApp.Models.VModels
+{
+    /// <summary>
+    /// 视图view_ExpendRecord的实体类
+    /// </summary>
+    [Table("view_ExpendRecord")]
+    [PrimaryKey("expendId")]
+    public class vExpendRecord
+    {
+        public int expendId { get; set; }
+        public string mName { get; set; }
+        public string cardNo { get; set; }
+        public string roomNo { get; set; }
+        public decimal expendAmount { get; set; }
+        public decimal payAmount { get; set; }
+        public DateTime expendTime { get; set; }
+    }
+}
